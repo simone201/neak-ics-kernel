610816f4af107507cf9134cb17e2acb5ea46a006
Simone Renzo <simone201@gmail.com>
2012-04-05 18:41:50 +0200

    vr: a bit tweaking for a bit more throughput
    makes almost no difference

--- block/vr-iosched.c
+++ block/vr-iosched.c
@@ -42,7 +42,7 @@ BACKWARD,
 
 static const int sync_expire = HZ / 2; /* max time before a sync is submitted. */
 static const int async_expire = 5 * HZ; /* ditto for async, these limits are SOFT! */
-static const int fifo_batch = 1;
+static const int fifo_batch = 2;
 static const int rev_penalty = 1; /* penalty for reversing head direction */
 
 struct vr_data {
