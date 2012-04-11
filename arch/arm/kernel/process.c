d800d2b66a858f736eab9c3c9556b868b3fce5ec
Simone Renzo <simone201@gmail.com>
2012-04-11 22:52:33 +0200

    Revert "ARM: clean up idle handlers"
    This reverts commit c5583c2b4cf07acc314110d41c7b3b6b7284078a.
    
    Conflicts:
    
    	arch/arm/kernel/process.c

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -198,17 +198,20 @@ void cpu_idle_wait(void)
 EXPORT_SYMBOL_GPL(cpu_idle_wait);
 
 /*
- * This is our default idle handler.
+ * This is our default idle handler.  We need to disable
+ * interrupts here to ensure we don't miss a wakeup call.
  */
-
-void (*arm_pm_idle)(void);
-
 static void default_idle(void)
 {
+<<<<<<< HEAD
 	if (arm_pm_idle)
 		arm_pm_idle();
 	else
 		cpu_do_idle();
+=======
+	if (!need_resched())
+		arch_idle();
+>>>>>>> parent of c5583c2... ARM: clean up idle handlers
 	local_irq_enable();
 }
 
@@ -235,10 +238,6 @@ void cpu_idle(void)
 				cpu_die();
 #endif
 
-			/*
-			 * We need to disable interrupts here
-			 * to ensure we don't miss a wakeup call.
-			 */
 			local_irq_disable();
 #ifdef CONFIG_PL310_ERRATA_769419
 			wmb();
@@ -246,17 +245,18 @@ void cpu_idle(void)
 			if (hlt_counter) {
 				local_irq_enable();
 				cpu_relax();
-			} else if (!need_resched()) {
+			} else {
 				stop_critical_timings();
 				pm_idle();
 				start_critical_timings();
 				/*
-				 * pm_idle functions must always
-				 * return with IRQs enabled.
+				 * This will eventually be removed - pm_idle
+				 * functions should always return with IRQs
+				 * enabled.
 				 */
 				WARN_ON(irqs_disabled());
-			} else
 				local_irq_enable();
+			}
 		}
 		tick_nohz_restart_sched_tick();
 		idle_notifier_call_chain(IDLE_END);
